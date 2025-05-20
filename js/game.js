c8ed576e7c15e87cc50950064a3f02d143a2c9cc
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-05-20 14:57:49 +0000

    Testing

---
+++ js/game.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
+//
+// Created by: Anastasiia Lebedenko
+// Created on: May 2025
+// This file contains the JS functions for index.html
+// global Phaser
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    backgroundColor: 0x5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
