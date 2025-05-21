d6d9cf2c19fea2e6699cb7f75c9fe4adee74e315
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-05-21 14:15:28 +0000

    2nd update to page

--- js/game.js
+++ js/game.js
@@ -4,12 +4,24 @@
 // Created on: May 2025
 // This file contains the JS functions for index.html
 // global Phaser
+"use strict";
 
 const config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
-    backgroundColor: 0x5f6e7a
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    // set background color
+    backgroundColor: 0x5f6e7a,
+    scale: {
+     mode: Phaser.Scale.FIT,
+     autoCenter: Phaser.Scale.CENTER_BOTH
+    }
 }
 
 const game = new Phaser.Game(config)
