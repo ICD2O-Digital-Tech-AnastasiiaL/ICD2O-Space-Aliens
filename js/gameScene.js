402f4a404acb56614138411c81e7164fc425ddf6
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-05-28 14:55:25 +0000

    pls work 8th update

--- js/gameScene.js
+++ js/gameScene.js
@@ -8,29 +8,29 @@
 class GameScene extends Phaser.Scene {
 
     constructor() {
-        super({ key: "gameScene" })
+        super({ key: 'gameScene' })
 
         this.background = null
         this.ship = null
     }
 
     init(data) {
-      this.cameras.main.setBackgroundColor("ffffff")
+      this.cameras.main.setBackgroundColor('#ffffff')
     }
     
     preload() {
-        console.log("Game Scene")
+        console.log('Game Scene')
 
         //images
-        this.load.image("startBackground", "assets/startBackground.png")
-        this.load.image("ship", "assets/spaceShip.png")
+        this.load.image('startBackground', 'assets/startBackground.png')
+        this.load.image('ship', 'assets/spaceShip.png')
     }
 
     create(data) {
-        this.background = this.add.image(0, 0, "startBackground").setScale(2.0)
+        this.background = this.add.image(0, 0, 'startBackground').setScale(2.0)
         this.background.setOrigin(0, 0)
 
-        this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+        this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
     }
 
     update(time, delta) {
